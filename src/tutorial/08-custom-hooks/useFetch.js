f011e355ce83b130568ee251c3b4651f3a560c6b
szvitek <gy.szvitek@gmail.com>
2023-04-17 16:31:41 +0100

    feat(customHooks): custom hooks exercises

---
+++ src/tutorial/08-custom-hooks/useFetch.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = (url) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await fetch(url);
+        // console.log(resp);
+        if (!resp.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          return;
+        }
+
+        const json = await resp.json();
+        setData(json);
+      } catch (error) {
+        setIsError(true);
+        // console.log(error);
+      }
+      // hide loading
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return {
+    isLoading,
+    isError,
+    data,
+  };
+};
+
+export default useFetch;
