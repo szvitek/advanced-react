9fabfd5ddc21a016c9a1566931835bb47526303c
szvitek <gy.szvitek@gmail.com>
2023-04-17 01:39:00 +0100

    feat(useEffect): useEffect cleanup function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/03-conditional-rendering/07-user-challenge';
+import Starter from './tutorial/02-useEffect/05-cleanup-function';
 
 function App() {
   return (
--- src/tutorial/02-useEffect/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/05-cleanup-function.jsx
@@ -1,5 +1,52 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  console.log('render');
+
+  return (
+    <div>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        toggle component
+      </button>
+      {toggle && (
+        <>
+          <RandomComponent />
+          <RandomComponent2 />
+        </>
+      )}
+    </div>
+  );
+};
+
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log('hmm, this is interesting');
+    const int = setInterval(() => {
+      console.log('hello from interval');
+    }, 1000);
+
+    return () => {
+      clearInterval(int);
+      console.log('cleanup');
+    };
+  }, []);
+
+  return <h1>hello there</h1>;
+};
+
+const RandomComponent2 = () => {
+  useEffect(() => {
+    const someFunc = () => {
+      // some logic
+    };
+    window.addEventListener('scroll', someFunc);
+
+    return () => window.removeEventListener('scroll', someFunc);
+  }, []);
+
+  return <h1>hello there</h1>;
 };
 
 export default CleanupFunction;
